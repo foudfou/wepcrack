110a22e611b96a6be3d19b5bc60e72ad835fcc20
foudfou <foudil.newbie+git@gmail.com>
2016-08-02 04:58:49 +0200

    Turn cartesian to iterative version.

--- cartesian.c
+++ cartesian.c
@@ -12,46 +12,42 @@ struct context
     int take;
 };
 
-void all(char *s, struct context *ctx);
 void reverse_print(char *s, struct context *ctx);
 
 
 int main(int argc, char **argv)
 {
     struct context ctx;
-    ctx.alpha = argv[1];
+    ctx.alpha = "ABCDEF";
     ctx.alpha_len = strlen(ctx.alpha);
-    ctx.take = atoi(argv[2]);
+    ctx.take = 4;
+
     char pw[ctx.take];           // char as numbers
     for (int r = 0; r < ctx.take; r++) pw[r] = 0;
 
     reverse_print(pw, &ctx);
-    all(pw, &ctx);
+
+    for (;;) {
+        int end = 0;
+        for (int k = ctx.take - 1; k >= 0; k--)
+            if (pw[k] == ctx.alpha_len - 1) end++;
+        if (end == ctx.take) {
+            reverse_print(pw, &ctx);
+            printf("\nAccomplisshed!\n");
+            break;
+        }
+
+        pw[0] += 1;
+        for (int i = 0; i < ctx.take - 1; i++) {
+            if (pw[i] == ctx.alpha_len) {
+                pw[i] = 0;
+                pw[i+1] += 1;
+            }
+        }
+        reverse_print(pw, &ctx);
+    }
 
     return 0;
-}
-
-void all(char *s, struct context *ctx)
-{
-    /* Basic Case */
-    int end = 0;
-    for (int k = ctx->take - 1; k >= 0; k--)
-        if (s[k] == ctx->alpha_len - 1) end++;
-    if (end == ctx->take) {
-        reverse_print(s, ctx);
-        printf("\nAccomplisshed!\n");
-        return;
-    }
-
-    s[0] += 1;
-    for (int i = 0; i < ctx->take - 1; i++) {
-        if (s[i] == ctx->alpha_len) {
-            s[i] = 0;
-            s[i+1] += 1;
-        }
-    }
-    reverse_print(s, ctx);
-    all(s, ctx);
 }
 
 /* This only prints. */
