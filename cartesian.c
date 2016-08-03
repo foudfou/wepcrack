eb5040301973ed9da91f170c0dd3e56445f8e5c6
foudfou <foudil.newbie+git@gmail.com>
2016-08-04 01:30:02 +0200

    * Use plain char's for bytes. * Reverse the main loop's order: print then increment.
    When using char's, we need to check the overflow *before* it happens.

--- cartesian.c
+++ cartesian.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -6,10 +7,10 @@
 // http://stackoverflow.com/a/13370778/421846
 
 /* The alphabet consists of bytes [0,255] */
-static const unsigned int UPPER = UCHAR_MAX;
-static const unsigned int TAKE = 3;
+static const unsigned char UPPER = UCHAR_MAX;
+static const unsigned char TAKE = 3;
 
-void print_pw(unsigned int *s)
+void print_pw(unsigned char *s)
 {
     for (int i = 0; i < TAKE; i++){
         printf("%02x ", s[i]);
@@ -20,33 +21,49 @@ void print_pw(unsigned int *s)
 
 int main(int argc, char **argv)
 {
-    /* Type must be large enough to contain alphabet + 1. Otherwise, we run
-       into overflow issues. */
-    unsigned int pw[TAKE];
-    for (int r = 0; r < TAKE; r++) pw[r] = 0;
-
-    print_pw(pw);
+    unsigned char pw[TAKE];
+    bool carry[TAKE];
+    for (int r = 0; r < TAKE; r++) {
+        pw[r] = 0;
+        carry[r] = 0;
+    }
 
     for (;;) {
-        int end = 0;
-        for (int k = 0; k < TAKE; k++)
-            if (pw[k] == UPPER) end++;
-        if (end == TAKE) {
-            print_pw(pw);
-            printf("\nAccomplisshed!\n");
-            break;
+
+        print_pw(pw);
+
+        if (pw[TAKE-1] + 1 > UPPER) {
+            pw[TAKE-1] = 0;
+            carry[TAKE-2] = true;
+        }
+        else {
+            pw[TAKE - 1] += 1;
         }
 
-        pw[TAKE-1] += 1;
-
-        for (int i = TAKE - 1; i > 0; i--) {
-            if (pw[i] == UPPER + 1) {
-                pw[i] = 0;
-                pw[i-1] += 1;
+        for (int i = TAKE - 2; i > 0; i--) {
+            if (carry[i]) {
+                if (pw[i] + 1 > UPPER) {
+                    pw[i] = 0;
+                    carry[i-1] = true;
+                }
+                else {
+                    pw[i] += 1;
+                }
+                carry[i] = false;
             }
         }
 
-        print_pw(pw);
+        if (carry[0]) {
+            if (pw[0] + 1 > UPPER) {
+                printf("\nAccomplisshed!\n");
+                break;
+            }
+            else {
+                pw[0] += 1;
+            }
+            carry[0] = false;
+        }
+
     }
 
     return 0;
