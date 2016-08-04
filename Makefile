b16dcb8fd847e2967f3817d780f1d679475a28bc
foudfou <foudil.newbie+git@gmail.com>
2016-08-04 23:03:41 +0200

    Failed attempt at openmp parallelisation.

--- Makefile
+++ Makefile
@@ -6,20 +6,20 @@ has_clang = $(shell clang --version 2> /dev/null)
 ifeq (, $(has_clang))
 	CC = gcc
 endif
-CFLAGS = -std=c11 -g -pedantic
-LDFLAGS =
+CFLAGS = -std=c11 -g -pedantic -fopenmp
+LDFLAGS = -lm -fopenmp
 
 all: $(EXE)
 	@printf "$(EXE)\n"
 
 $(EXE): $(OBJ)
-	$(CC) -o $@ $^ $(LDFLAGS)
 	@printf "\e[33mLinking\e[0m %s\n" $@
+	$(CC) -o $@ $^ $(LDFLAGS)
 	@printf "\e[34mDone!\e[0m\n"
 
 %.o: %.c
-	$(CC) -o $@ -c $< $(CFLAGS)
 	@printf "\e[36mCompile\e[90m %s\e[0m\n" $@
+	$(CC) -o $@ -c $< $(CFLAGS)
 
 .PHONY: clean mrproper
 
--- cartesian.c
+++ cartesian.c
@@ -1,17 +1,20 @@
+#include <limits.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <string.h>
+#include <omp.h>
 
 // http://stackoverflow.com/a/13370778/421846
 
 /* The alphabet consists of bytes [0,255] */
-static const unsigned char UPPER = UCHAR_MAX;
+static const unsigned char UPPER = 9; // UCHAR_MAX;
 static const unsigned char TAKE = 3;
 
-void print_pw(unsigned char *s)
+void print_pw(unsigned char *s, int ithr)
 {
+    printf("%u: ", ithr);
     for (int i = 0; i < TAKE; i++){
         printf("%02x ", s[i]);
     }
@@ -28,43 +31,71 @@ int main(int argc, char **argv)
         carry[r] = 0;
     }
 
-    for (;;) {
+    long long glob_iter =
+        (long long)pow((double)(UPPER + 1), (double)TAKE);
+    printf("total glob_iter:  %lli\n", glob_iter);
+    long long k;
 
-        print_pw(pw);
+#pragma omp parallel private(pw, k) firstprivate(carry)
+    {
+        long long thr_iter = 0;
 
-        if (pw[TAKE-1] + 1 > UPPER) {
-            pw[TAKE-1] = 0;
-            carry[TAKE-2] = true;
-        }
-        else {
-            pw[TAKE - 1] += 1;
-        }
+        // pw needs to be initialized here like
+        const int nthreads = omp_get_num_threads();
+        const int ithread = omp_get_thread_num();
+        const long long start = glob_iter * ithread / nthreads;
+        const long long finish = glob_iter * (ithread + 1) / nthreads;
 
-        for (int i = TAKE - 2; i > 0; i--) {
-            if (carry[i]) {
-                if (pw[i] + 1 > UPPER) {
-                    pw[i] = 0;
-                    carry[i-1] = true;
-                }
-                else {
-                    pw[i] += 1;
-                }
-                carry[i] = false;
-            }
+        for (int r = 0; r < TAKE; r++) {
+            pw[r] = start % (long long)pow((double)(UPPER + 1), (double)(TAKE-r));
         }
 
-        if (carry[0]) {
-            if (pw[0] + 1 > UPPER) {
-                printf("\nAccomplisshed!\n");
-                break;
+#pragma omp for schedule(monotonic:static)
+        for (long long k = start; k < finish; k++) {
+            ++thr_iter;
+
+#pragma omp critical
+            print_pw(pw, ithread);
+
+            if (pw[TAKE-1] + 1 > UPPER) {
+                pw[TAKE-1] = 0;
+                carry[TAKE-2] = true;
             }
             else {
-                pw[0] += 1;
+                pw[TAKE-1] += 1;
             }
-            carry[0] = false;
+
+            for (int i = TAKE - 2; i > 0; i--) {
+                if (carry[i]) {
+                    if (pw[i] + 1 > UPPER) {
+                        pw[i] = 0;
+                        carry[i-1] = true;
+                    }
+                    else {
+                        pw[i] += 1;
+                    }
+                    carry[i] = false;
+                }
+            }
+
+            if (carry[0]) {
+                if (pw[0] + 1 > UPPER) {  /* FIXME: limit should be finish */
+                    printf("\nAccomplisshed!\n");
+                    /* break; */
+                    #pragma omp cancel for
+                }
+                else {
+                    pw[0] += 1;
+                }
+                carry[0] = false;
+            }
+
+#pragma omp cancellation point for
         }
 
-    }
+        printf("Thread %u: %lli iterations completed\n", ithread, thr_iter);
+
+    } // end omp parallel
 
     return 0;
 }
